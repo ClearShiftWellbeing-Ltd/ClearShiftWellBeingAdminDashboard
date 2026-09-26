a343cdb7a14601110575bce6da7972a43dbefdd0
ClearShiftWellbeing <hello@clearshiftwellbeing.co.uk>
2026-09-26 09:32:18 +0100

    Refactor next.config.ts to update webpack and ESLint
    Removed custom webpack configuration for SVG handling and added ESLint configuration to ignore errors during builds.

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-    return config;
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
